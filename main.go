af1a54ae4c4a9f18537ed3fbf7820c327f1c4201
tursynkhan <tursynkhan001@gmail.com>
2022-07-05 16:17:32 +0600

    delete \n

--- main.go
+++ main.go
@@ -7,21 +7,22 @@ import (
 	"log"
 )
 func main(){
-	if len(os.Args)!=2{
-		fmt.Println("Not correct number of arguments")
-		return
-	}
-	args := []rune(os.Args[1])
-	err:=Isvalid(args)
-	if err{
-		return
-	}
-	symbols:=Read()
-	PrintArt(args,symbols)
-	Isvalid(args)
+	// if len(os.Args)!=2{
+	// 	fmt.Println("Not correct number of arguments")
+	// 	return
+	// }
+	// args := []rune(os.Args[1])
+	// err:=Isvalid(args)
+	// if err{
+	// 	return
+	// }
+	args:="hello"
+	symbols:=ReadBanner()
+	ReadArgs(args,symbols)
+	
 }
 
-func Read()(map[int][]string){
+func ReadBanner()(map[int][]string){
 	symbols:=make(map[int][]string)
 	var buf []string
 	counter:=0
@@ -48,22 +49,44 @@ func Read()(map[int][]string){
 	}
 	return symbols
 }
-func PrintArt(args []rune, symbols map[int][]string){
+func ReadArgs(args []rune, symbols map[int][]string){
 	output:= make([]string,8)
 	for  k:=0;k<len(args);k++{
+		if args[k]=='\n'{
+			output=PrintArt(output)
+			continue
+		}
+		if  len(args)!=k+1 && (args[k]=='\\' && args[k+1]=='n'){
+			output=PrintArt(output)
+			continue
+		}
 		for i,w:=range symbols[int(args[k])]{
 			output[i]+=w
 		}
+		// if len(args)<k+1{
+		// 	output=PrintArt(output)
+		// 	break
+		// }
 	}
-	for _,r:=range output{
-		fmt.Println(r)
+
+}
+
+func PrintArt(output []string)[]string{	
+	if output[0]!=""{
+		for i,w:=range output{
+			fmt.Println(w)
+			output[i]=""
+		}
+		return output
 	}
+	fmt.Println()
+	return output
 }
 
 func Isvalid(args []rune)bool{
 	for _,r:=range args{
-		if (r<32 || r>127){
-			fmt.Println("Please, input onli ascii character!")
+		if (r<32 || r>127)&&r!=10{
+			fmt.Println("Please, input only ascii character!")
 			return true
 		}
 	}
