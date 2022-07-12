103915541560f98b767fda76023e5a767f49e7e9
tursynkhan <student@ALEM-3170f9.alem.school>
2022-07-12 20:48:08 +0600

    add argument

--- main.go
+++ main.go
@@ -7,16 +7,18 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 2 {
+	if len(os.Args) != 3 {
 		fmt.Println("Not correct number of arguments")
 		return
 	}
+	banners := "banners/" + os.Args[2] + ".txt"
+
 	args := []rune(os.Args[1])
 	err := src.Isvalid(args)
 	if err {
 		return
 	}
 
-	symbols := src.ReadBanner()
+	symbols := src.ReadBanner(banners)
 	src.ReadArgs(args, symbols)
 }
