7652da27169d01517be0072d7d2fb928d143c3b3
tursynkhan <student@ALEM-3170f9.alem.school>
2022-07-12 14:59:26 +0600

    newline

--- src/readargs.go
+++ src/readargs.go
@@ -1,7 +1,10 @@
 package src
 
+import "fmt"
+
 func ReadArgs(args []rune, symbols map[int][]string) {
 	output := make([]string, 8)
+	buf := false
 	for k := 0; k < len(args); k++ {
 		if args[k] == '\n' {
 			output = PrintArt(output)
@@ -12,12 +15,18 @@ func ReadArgs(args []rune, symbols map[int][]string) {
 			k++
 			continue
 		}
+		buf = true
 		for i, w := range symbols[int(args[k])] {
 			output[i] += w
 		}
 		if k+1 >= len(args) {
+			buf = false
 			PrintArt(output)
 			break
 		}
+	}
+	if buf {
+		fmt.Println()
+		return
 	}
 }
