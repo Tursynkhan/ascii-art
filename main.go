a782cc8b79df2b0f8c50d8c41b9326d4ada6bc66
tursynkhan <student@ALEM-316ed2.alem.school>
2022-07-13 19:57:00 +0600

    add fs

--- main.go
+++ main.go
@@ -9,10 +9,15 @@ import (
 func main() {
 	if len(os.Args) != 3 {
 		fmt.Println("Not correct number of arguments")
+		fmt.Println("The arguments should be : go run . [STRING] [BANNER]")
 		return
 	}
 	banners := "banners/" + os.Args[2] + ".txt"
 
+	if os.Args[2] != "standard" && os.Args[2] != "shadow" && os.Args[2] != "thinkertoy" {
+		fmt.Println("[BANNER] should be : standard, shadow, thinkertoy")
+		return
+	}
 	args := []rune(os.Args[1])
 	err := src.Isvalid(args)
 	if err {
