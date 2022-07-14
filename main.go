cd0c851f9734935f3b61bde4612cb8d66553c046
tursynkhan001@gmail.com <student@ALEM-316ed2.alem.school>
2022-07-14 19:37:53 +0600

    change

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
+	"log"
 	"main/src"
 	"os"
 )
 
 func main() {
-	banners := "banners/" + os.Args[2] + ".txt"
 	src.IsvalidArgs()
 	args := []rune(os.Args[1])
 	err := src.Isvalid(args)
 	if err {
 		return
 	}
-
-	symbols := src.ReadBanner(banners)
+	symbols, err1 := src.ReadBanner()
+	if err1 != nil {
+		log.Println(err1)
+		return
+	}
 	src.ReadArgs(args, symbols)
 }
