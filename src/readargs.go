db2203ead9c2105e3d55db4f802156306bad25b0
tursynkhan <student@ALEM-316ed2.alem.school>
2022-07-14 14:41:33 +0600

    add exec command

--- src/readargs.go
+++ src/readargs.go
@@ -3,32 +3,49 @@ package src
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"strconv"
+	"strings"
 )
 
 func ReadArgs(args []rune, symbols map[int][]string) {
+	cmd := exec.Command("stty", "size")
+	cmd.Stdin = os.Stdin
+	out, _ := cmd.Output()
+	size := strings.Split(string(out), " ")
+	width, _ := strconv.Atoi(size[1][:len(size[1])-1])
+	counter := 0
+
 	output := make([]string, 8)
-	buf := false
+	newline := false
 	for k := 0; k < len(args); k++ {
 		if args[k] == '\n' {
 			output = PrintArt(output)
+			counter = 0
 			continue
 		}
 		if len(args) != k+1 && (args[k] == '\\' && args[k+1] == 'n') {
 			output = PrintArt(output)
+			counter = 0
 			k++
 			continue
 		}
-		buf = true
+		newline = true
+		if counter+len(symbols[int(args[k])][0]) >= width {
+			output = PrintArt(output)
+			counter = 0
+		}
 		for i, w := range symbols[int(args[k])] {
 			output[i] += w
 		}
+		counter += len(symbols[int(args[k])][0])
 		if k+1 >= len(args) {
-			buf = false
+			newline = false
 			PrintArt(output)
 			break
 		}
 	}
-	if buf {
+	if newline {
 		fmt.Println()
 		return
 	}
